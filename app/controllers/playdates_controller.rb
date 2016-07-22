a6229dcffd98b01a61580f53d4d3b6d665294e3d
Amanda Lin <adnama.lin@gmail.com>
2016-07-22 14:00:12 -0500

    can add playdate event

--- app/controllers/playdates_controller.rb
+++ app/controllers/playdates_controller.rb
@@ -5,4 +5,22 @@ class PlaydatesController < ApplicationController
     @pending = User.find(params[:user_id]).pending_playdates
   end
 
+  def new
+    @playdate = Playdate.new
+  end
+
+  def create
+    @playdate = Playdate.new(playdate_params)
+    if @playdate.save
+      redirect_to user_playdates_path(current_user.id)
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def playdate_params
+    params.require(:playdate).permit(:title, :description, :date,:location).merge(host_id: current_user.id)
+  end
+
 end
