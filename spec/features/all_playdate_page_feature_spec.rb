2557f503c46fbd536b095336094be6faf4a063c7
Amanda Lin <adnama.lin@gmail.com>
2016-07-22 11:23:30 -0500

    pass first two tests

--- spec/features/all_playdate_page_feature_spec.rb
+++ spec/features/all_playdate_page_feature_spec.rb
@@ -1,17 +1,19 @@
 require 'rails_helper'
 
 feature "view all the songs on the home page" do
-  before(:each) { User.create!(username:"amanda", password:"1234", email: "adnama.lin@gmail.com")}
-  before(:each) {Playdate.create!(title: "Brunch at Benjis", description: "Let's eat brunch and drink bloddy mary's while the kids play", location:"123 Main St. Chicago, Il", date:"2016-07-28" ,host_id: 1) }
-
 
   scenario "see scheduled playdates" do
-    visit '/users/:id/playdates'
-      expect(page).to have_content "July, 28th with Benji and Jax"
+    user = User.create!(username:"amanda", password:"1234", email: "adnama.lin@gmail.com")
+    Playdate.create!(title: "Brunch at Benjis", description: "Let's eat brunch and drink bloddy mary's while the kids play", location:"123 Main St. Chicago, Il", date:"2016-07-28" ,host_id: user.id)
+    visit "/users/#{user.id}/playdates"
+    within(".playdate-listing") do
+      expect(page).to have_content "July, 28th | Brunch at Benjis"
+    end
   end
 
   scenario "click on a link to view all playdates" do
-    visit '/users/:id/playdates'
+    user = User.create!(username:"amanda", password:"1234", email: "adnama.lin@gmail.com")
+    visit "/users/#{user.id}/playdates"
     find_link("See All Playdates").visible?
   end
 end
