6fa1ea03bdfb8d55117da803a33b7930c262f875
Leland Meiners <lelandam@gmail.com>
2016-07-22 17:40:41 -0500

    hooking up some paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#new'
+    root 'sessions#new'
+
 
   get '/kinders/reset' => 'kinders#reset_kinders', as:"reset_kinders"
   get '/login' => 'sessions#new'
