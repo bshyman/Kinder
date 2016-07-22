dc9bb118d20cdbc50dbb3a9d8a369483417ae811
Amanda Lin <adnama.lin@gmail.com>
2016-07-22 10:37:16 -0500

    add attendees table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160722152447) do
+ActiveRecord::Schema.define(version: 20160722153525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attendees", force: :cascade do |t|
+    t.integer "guest_id"
+    t.integer "playdate_id"
+    t.boolean "response"
+  end
 
   create_table "playdates", force: :cascade do |t|
     t.string   "title",       null: false
