3013caf8db776f6a8e51f1de9468bdd45f6a5327
Benjamin Shyman <bshyman@gmail.com>
2016-07-24 16:02:36 -0500

    profile upload works maybe, try on heroku

--- app/models/zipcodeapi.rb
+++ app/models/zipcodeapi.rb
@@ -3,17 +3,13 @@ require 'httparty'
 class ZipcodeAPI
 
   def initialize
-    @api_key = "40MbMr3aoCHonOpf6zFb7Ymbr7hUdlSilEu86yjvb1TyPjNWTs81rucSCuNQQF9c"
+    @api_key = ENV['ZIPCODEAPIKEY']
   end
 
 
   def get_nearby_zipcodes(zipcode, radius_preference)
-    p "PRINTINGGGG"
-    p radius_preference
-    p zipcode
     response = HTTParty.get("https://www.zipcodeapi.com/rest/#{@api_key}/radius.json/#{zipcode}/#{radius_preference}/mile")
     nearby_zipcodes = []
-    p response
     response["zip_codes"].each do |z|
       nearby_zipcodes << z["zip_code"]
     end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,12 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => 'AKIAJFV2YNLFWDEKV2PQ',
-    :aws_secret_access_key  => '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',
+    :aws_access_key_id      => ENV['AWSACCESSKEYID'],
+    :aws_secret_access_key  => ENV['AWSSECRETKEY'],
     :region                 => 'us-east-1'
   }
   config.fog_directory  = 'kinderprofilepics'
   config.fog_public     = true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
 end
+
