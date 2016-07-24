551d72db080078b4966c60b6ea994ed58d0555bd
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-07-24 12:45:33 -0500

    found issue with api key, the key expires every day...

--- app/models/zipcodeapi.rb
+++ app/models/zipcodeapi.rb
@@ -3,13 +3,17 @@ require 'httparty'
 class ZipcodeAPI
 
   def initialize
-    @api_key = "t6U2dKZo0mAZ8rMDi62mO71EX2bicr5GxZILKHaK3KKcbwPtCRQrZR6WUhf6JkMg"
+    @api_key = "40MbMr3aoCHonOpf6zFb7Ymbr7hUdlSilEu86yjvb1TyPjNWTs81rucSCuNQQF9c"
   end
 
 
   def get_nearby_zipcodes(zipcode, radius_preference)
+    p "PRINTINGGGG"
+    p radius_preference
+    p zipcode
     response = HTTParty.get("https://www.zipcodeapi.com/rest/#{@api_key}/radius.json/#{zipcode}/#{radius_preference}/mile")
     nearby_zipcodes = []
+    p response
     response["zip_codes"].each do |z|
       nearby_zipcodes << z["zip_code"]
     end
